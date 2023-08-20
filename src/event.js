ffb2d6124b837f555c940daa0f44e7395ffadaa1
Martin Clementz <martcl@stud.ntnu.no>
2023-08-20 19:55:36 +0200

    chore: more tests on event properties (#2)

--- src/event.js
+++ src/event.js
@@ -1,25 +1,25 @@
 const { isDate, SPOND_API_URL } = require('./utils');
 
 /**
- * @typedef {import("./group").Group} Group
- * @typedef {import("./group").SubGroup} SubGroup
- */
-
-/**
  * @typedef {Object} Event - Represents an event.
  * @property {string} id
  * @property {string} heading
  * @property {string} creatorId
  * @property {string} description
- * @property {Array<any>} owners
+ * @property {string} [picture]
+ * 
+ * @property {Array<EventOwner>} owners
+ * 
  * @property {Date} startTimestamp
  * @property {Date} endTimestamp
- * @property {string} location
- * @property {{group: Group profiles: Array<any>, guardians: Array<any>}} recipients
- * @property {{acceptedIds: Array<string>, declinedIds: Array<string>, unanswerdIds: Array<string>, waitinglistIds: Array<string>}} responses
- * @property {{openTasks: Array<any>, assignedTasks: Array<any>}} tasks
+ * @property {Date} [meetupTimestamp]
+ * 
+ * @property {Location} [location]
+ * @property {{group: EventGroup, profiles: Array<any>, guardians: Array<any>}} recipients
+ * @property {EventResponses} responses
+ * @property {{openTasks: Array<OpenTasks>, assignedTasks: Array<any>}} tasks
  * @property {Array<any>} comments
- * @property {Array<any>} attachments
+ * @property {Array<Attachment>} attachments
  * @property {Date} createdTime
  * @property {boolean} expired
  * @property {string} visibility
@@ -32,6 +32,70 @@ const { isDate, SPOND_API_URL } = require('./utils');
  * @property {string} type
  * @property {string} updated TODO: Find out what this time is
  * @property {boolean} matchEvent TODO: Find out what this is
+ * 
+ * @typedef {Object} EventOwner - Represents a event owner.
+ * @property {string} id
+ * @property {string} response
+ * 
+ * @typedef {Object} SubGroup - Represents a sub group.
+ * @property {string} id
+ * @property {string} name
+ * @property {string} color
+ * 
+ * @typedef {Object} Member - Represents a event recipient.
+ * @property {string} id
+ * @property {string} firstName
+ * @property {string} lastName
+ * @property {string} respondent
+ * @property {Array<any>} guardians
+ * 
+ * @typedef {Object} EventGroup - Represents a event group.
+ * @property {string} id
+ * @property {string} contactPersonId
+ * @property {string} name
+ * @property {Array<SubGroup>} subGroups
+ * @property {number} createdTime
+ * @property {Array<Member>} members
+ * @property {number} type
+ * @property {string} activity
+ * 
+ * @typedef {Object} EventResponses - Represents event responses.
+ * @property {Array<string>} acceptedIds
+ * @property {Array<string>} declinedIds
+ * @property {Array<string>} unansweredIds
+ * @property {Array<string>} waitinglistIds
+ * @property {Array<string>} unconfirmedIds
+ * 
+ * @typedef {Object} OpenTasks - Represents open tasks.
+ * @property {string} id
+ * @property {string} name
+ * @property {string} description
+ * @property {boolean} adultsOnly
+ * @property {string} type
+ * @property {number} limit - The maximum number of participants.
+ * @property {number} remaining - The number of remaining participant that is needed to complete the task.
+ * @property {Array<string>} accepted
+ * @property {Array<string>} declined
+ * @property {Array<string>} unanswered
+ * 
+ * @typedef {Object} Attachment - Represents an attachment.
+ * @property {string} id
+ * @property {string} media
+ * @property {number} type
+ * @property {string} title
+ * @property {string} ownerId
+ * @property {Date} timestamp
+ * 
+ * @typedef {Object} Location - Represents a location.
+ * @property {string} id
+ * @property {string} feature
+ * @property {string} address
+ * @property {number} latitude
+ * @property {number} longitude
+ * @property {string} country
+ * @property {string} administrativeAreaLevel1
+ * @property {string} locality
+ * 
  */
 
 /**
@@ -48,8 +112,6 @@ const { isDate, SPOND_API_URL } = require('./utils');
  * @param {number} options.maxEvents - The maximum number of events to fetch (default is 50).
  * @throws {Error} Throws an error if any of the date options is not a valid Date object.
  * @returns {Promise<Array<Event>>} An array of event objects.
-
- *
  */
 async function getEvents(
   accessToken,
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -55,10 +55,50 @@ describe('Spond events', () => {
       groupId: groups.find((group) => group.name == 'My Spond group 1').id,
     });
 
-    expect(events).not.toBeUndefined();
-    expect(events[0].heading).not.toBeUndefined();
-    expect(events[0].description).not.toBeUndefined();
-    expect(events[0].creatorId).not.toBeUndefined();
-    expect(events[0].autoAccept).not.toBeUndefined();
+    const event = events.find((event) => event.heading == 'Test event 1');
+
+    expect(event).not.toBeUndefined();
+
+    // Basic info
+    expect(event.createdTime).toBe("2023-08-19T20:19:54.236Z")
+    expect(event.recipients.group.name).toBe("My Spond group 1")
+    expect(event.heading).toBe("Test event 1");
+    expect(event.description).toBe("This is a cool test event!");
+
+    // Time
+    expect(event.startTimestamp).toBe("2023-08-19T21:00:00Z");
+    expect(event.endTimestamp).toBe("2023-08-19T22:00:00Z");
+    expect(event.meetupTimestamp).toBe("2023-08-19T20:50:00Z");
+
+    // Location
+    expect(event.location.address).toBe("Trondheim");
+    expect(event.location.latitude).toBe(63.430515);
+    expect(event.location.longitude).toBe(10.395053);
+    expect(event.location.country).toBe("NO");
+    expect(event.location.administrativeAreaLevel1).toBe("Trøndelag");
+    expect(event.location.locality).toBe("Trondheim");
+
+    // Responses
+    expect(event.responses.acceptedIds).not.toBeUndefined();
+    expect(event.responses.declinedIds).not.toBeUndefined();
+    expect(event.responses.unansweredIds).not.toBeUndefined();
+    expect(event.responses.waitinglistIds).not.toBeUndefined();
+
+    // Group
+    expect(event.recipients.group.subGroups.length).toBe(2);
+    expect(event.recipients.group.subGroups.find(subgroup => subgroup.name === "Alfa").color).toBe("#0084ff");
+    expect(event.recipients.group.subGroups.find(subgroup => subgroup.name === "Beta").color).toBe("#ff5ca1");
+    expect(event.recipients.group.members.filter(member => member.firstName === "Martin").length).toBe(1);
+
+    // Tasks
+    expect(event.tasks.openTasks.length).toBe(1);
+    expect(event.tasks.openTasks[0].name).toBe("This is a task");
+    expect(event.tasks.openTasks[0].description).toBe("This is a description of the task");
+
+    // Other
+    expect(event.creatorId).not.toBeUndefined();
+    expect(event.autoAccept).not.toBeUndefined();
+    expect(event.expired).toBe(true);
+
   });
 });
