215dde8e5db8509b0a565a214588718be255a156
martcl <martcl@users.noreply.github.com>
2024-05-04 05:45:09 +0000

    deploy: 22ca432d784183b372e00ecbf47ed1d87f8e8326

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,9 +1,15 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
+  let hostname = location.hostname; 
 
+  let path = "/@typespec/openapi3/openapi.yaml";
+
+  if (hostname === "martcl.github.io") {
+    path = "/spond" + path;
+  }
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./@typespec/openapi3/openapi.yaml",
+    url: path,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
